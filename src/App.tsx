b8b2d2e2845c2c1cb09053ef3a9cf7ab58f505f8
Mina Malak <mina.malak@nexspec.com>
2026-06-17 14:36:15 +0300

    feat: first trial

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,30 @@ import AppHeader from "./components/AppHeader";
 import AppFooter from "./components/AppFooter";
 import JsonProcessor from "./features/jsonProcessor/JsonProcessor";
 
-import JsonProcessorPanel from "./layouts/JsonProcessorPanel";
-
-function App() {
-  return <JsonProcessorPanel />;
+/** Application shell: header, the processing workspace, and footer. */
+export default function App() {
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "background.default",
+      }}
+    >
+      <AppHeader />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          minHeight: 0,
+          px: { xs: 2, md: 4 },
+          py: { xs: 2, md: 3 },
+        }}
+      >
+        <JsonProcessor />
+      </Box>
+      <AppFooter />
+    </Box>
+  );
 }
